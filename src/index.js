0233853383a8d57c1f50528d9648c3f063dd6e2f
Ammon Morris <likesalmon@gmail.com>
2020-11-25 08:16:25 -0900

    build(): add input

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 const axios = require("axios");
+const core = require("@actions/core");
 
 const run = async () => {
+  const id = core.getInput("id") || 1;
+
   const { name, climate, terrain } = await axios
-    .get("https://swapi.dev/api/planets/1/")
+    .get(`https://swapi.dev/api/planets/${id}/`)
     .then((res) => res.data);
   console.log(`${name} is an ${climate} ${terrain}.`);
 };
