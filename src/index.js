f3addbb2e166eb56658805460af65341e646e877
Ammon Morris <likesalmon@gmail.com>
2020-11-25 08:52:20 -0900

    build(): add debug statements

--- src/index.js
+++ src/index.js
@@ -6,15 +6,20 @@ const AVAILABLE_IDS = [1, 2, 3, 4];
 
 const run = async () => {
   const id = core.getInput("id") || DEFAULT_ID;
+  core.debug(`[CI] ID: ${id}`);
 
   if (!AVAILABLE_IDS.includes(id)) {
-    core.setFailed(`Unknown ID: ${id}`);
+    core.setFailed(`[CI] Unknown ID: ${id}`);
     return;
   }
 
-  const { name, climate, terrain } = await axios
-    .get(`https://swapi.dev/api/planets/${id}/`)
-    .then((res) => res.data);
+  core.debug(`[CI] Fetching planet with ID ${id}`);
+
+  const { data } = await axios.get(`https://swapi.dev/api/planets/${id}/`);
+
+  core.debug(`Retrieved: ${JSON.stringify(data)}`);
+
+  const { name, climate, terrain } = data;
   console.log(`${name} is an ${climate} ${terrain}.`);
 
   core.setOutput("planet", name);
